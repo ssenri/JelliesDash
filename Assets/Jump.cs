9b9ddd597b4e40199eee66ebae7b7be8874376c6
ssenri <shinou.9023@gmail.com>
2018-10-12 15:54:37 +0900

    shi tuika

---
+++ Assets/Jump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+
+    public GameObject Player;
+    private Rigidbody PlayerRigid;
+    public float Upspeed;
+
+
+	void Start ()
+    {
+        PlayerRigid = Player.GetComponent<Rigidbody>();
+	}
+
+	void Update ()
+    {
+
+	}
+
+    void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Ground" && Input.GetKeyDown(KeyCode.Space)) 
+        {
+            PlayerRigid.AddForce(transform.up * Upspeed);
+        }
+    }
+}
---
+++ Assets/P.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class P : MonoBehaviour
+{
+    float speed = 3.0f;
+    float st = 100.0f;
+    Slider _slider;
+    int n = 2;
+
+    void Start()
+    {
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+
+
+    void Update()
+    {
+        Move();
+
+        if(Input.GetKey(KeyCode.B))
+        {
+            transform.position += transform.right * speed * n * Time.deltaTime;
+            st -= 0.5f;
+        }
+        _slider.value = st;
+
+        if (st <= 0)
+        {
+            n = 0;
+        }
+    }
+
+    void Move()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+    }
+}
